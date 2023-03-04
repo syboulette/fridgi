3a287b0c1120f1fc34e2adf1199efb0b54ba3eee
Sybille Lambotte <lambottesybille@gmail.com>
2023-03-04 11:28:16 +0100

    controller generation

---
+++ app/controllers/fridges_controller.rb
@@ -0,0 +1,2 @@
+class FridgesController < ApplicationController
+end
---
+++ test/controllers/fridges_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class FridgesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
