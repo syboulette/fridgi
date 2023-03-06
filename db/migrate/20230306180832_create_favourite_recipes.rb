72c74329b584e0cc3a422ca0df8cc7a64bcac80d
Vinicius Batestin <viniciusbatestin@gmail.com>
2023-03-06 20:18:47 +0100

    migration fix

--- db/migrate/20230306180832_create_favourite_recipes.rb
+++ db/migrate/20230306180832_create_favourite_recipes.rb
@@ -1,8 +1,8 @@
 class CreateFavouriteRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :favourite_recipes do |t|
-      t.references :recipe_id, null: false, foreign_key: true
-      t.references :user_id, null: false, foreign_key: true
+      t.references :recipe, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
