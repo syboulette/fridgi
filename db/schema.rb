341cb54cfec606663adba90d2e3ba9e66ee8ec99
Ariane Kramer <ariane.kramer@posteo.de>
2023-03-06 22:05:44 +0100

    added css center box and button and html for the homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_180832) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_132144) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,15 +67,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_180832) do
     t.string "preference"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "favourite_recipes", force: :cascade do |t|
-    t.bigint "recipe_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["recipe_id"], name: "index_favourite_recipes_on_recipe_id"
-    t.index ["user_id"], name: "index_favourite_recipes_on_user_id"
   end
 
   create_table "fridge_ingredients", force: :cascade do |t|
@@ -185,8 +176,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_180832) do
   add_foreign_key "dietary_information_recipes", "recipes"
   add_foreign_key "dietary_information_users", "dietary_informations"
   add_foreign_key "dietary_information_users", "users"
-  add_foreign_key "favourite_recipes", "recipes"
-  add_foreign_key "favourite_recipes", "users"
   add_foreign_key "fridge_ingredients", "fridges"
   add_foreign_key "fridge_ingredients", "ingredients"
   add_foreign_key "fridges", "users"
