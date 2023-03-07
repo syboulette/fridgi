a4a31cd21964732fef536941e8ba21881fe2982b
Ariane Kramer <ariane.kramer@posteo.de>
2023-03-07 21:58:31 +0100

    added view and form for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,22 @@
 class RecipesController < ApplicationController
+
+
+  def new
+    @recipe = Recipe.new
+    authorize @recipe
+  end
+
+  def create
+    @recipe = recipe.new(recipe_params)
+    @recipe.user = current_user
+
+    authorize @recipe
+
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end
