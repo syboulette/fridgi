a2d6353ce0e63e28c9bd972ad18247002b5362c7
Vinicius Batestin <viniciusbatestin@gmail.com>
2023-03-07 19:21:20 +0100

    Routes complete for now  and approved by TA

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   
   resources :users, only: [:show]
   resources :fridges, only: [:show, :create, :destroy, :edit]
-  resources :recipes, only: [:new, :create, :destroy, :index, :show]
-  resources :favorite_recipes, only: [:new, :create, :edit,]
-  get "/my_recipes", to: "user#recipes"
-  get "/favorite_recipes", to: "user#favorite_recipes"
-  resources :reviews, only: [:new, :create, :destroy, :edit]
+  resources :recipes, only: [:new, :create, :destroy, :index, :show] do 
+    colection do 
+      get :my_recipes
+    end
+    resources :reviews, only: [:new, :create, :destroy, :edit]
+  end
+  resources :favorite_recipes, only: [:new, :create, :edit, :index]
+
   resources :lists, only: [:show, :edit, :destroy]
 end
