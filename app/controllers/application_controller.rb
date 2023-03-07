0c2c18124c8e6bb72c52da66bd7d26dbbe5d702a
Dominic Zaleski <dominic.zaleski@outlook.com>
2023-03-07 18:15:53 +0100

    progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,12 +16,12 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :language])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :language, :address, :country, :city, :post_code])
 
     # address_attributes: [:country, :city, :post_code]
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :language])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :language, :address, :country, :city, :post_code])
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_131111) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_161705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -179,6 +179,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_131111) do
     t.string "last_name"
     t.string "phone_number"
     t.string "language"
+    t.string "address"
+    t.string "country"
+    t.string "city"
+    t.string "post_code"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
