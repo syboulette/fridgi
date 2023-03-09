b3e81e808ff7f9ce3d226a823c3460533ecce261
Ariane Kramer <ariane.kramer@posteo.de>
2023-03-09 19:53:25 +0100

    added pundit - recipe policy

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,17 @@
 class RecipesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
+
+  def index
+    @recipes = policy_scope(Recipe).all
+
+    if params[:query].present?
+      @recipes = Recipe.recipe_search(params[:query])
+    else
+      @recipes = Book.all
+    end
+  end
 
   def new
     @recipe = Recipe.new
@@ -19,4 +31,23 @@ class RecipesController < ApplicationController
     end
   end
 
+  def show
+    authorize @recipe
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :instruction, :total_time, :serving)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 end
---
+++ app/policies/recipe_policy.rb
@@ -0,0 +1,12 @@
+class RecipePolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+  def new?
+    true
+  end
+
+end
