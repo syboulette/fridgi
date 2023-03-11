2968ea5e0e32683988d5db1d7a7b4bb51d27fcfc
Ariane Kramer <ariane.kramer@posteo.de>
2023-03-11 17:37:32 +0100

    deleted the show recipe possibility for unauthenticated users

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index]
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
   def index
