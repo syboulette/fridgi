de203c82543304aa40de2d8bef60b508b3a7d41b
Ariane Kramer <ariane.kramer@posteo.de>
2023-03-11 18:40:58 +0100

    added recipe_ingredients to have to exist (validating presence)

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,5 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :dietary_information_recipes
 
-  validates_presence_of :total_time, :title, :serving, :instruction
+  validates_presence_of :total_time, :title, :serving, :instruction, :recipe_ingredients
 end
