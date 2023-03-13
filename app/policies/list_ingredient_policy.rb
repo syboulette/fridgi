5bd8cb9b94b4072cc0670a08de2d8224c38ca78a
Sybille Lambotte <lambottesybille@gmail.com>
2023-03-13 18:55:40 +0100

    dumm me forgot to save most of the files

--- app/policies/list_ingredient_policy.rb
+++ app/policies/list_ingredient_policy.rb
@@ -1,4 +1,4 @@
-class ListIngredientsPolicy < ApplicationPolicy
+class ListIngredientPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
--- app/policies/recipe_ingredient_policy.rb
+++ app/policies/recipe_ingredient_policy.rb
@@ -1,4 +1,4 @@
-class RecipeIngredientsPolicy < ApplicationPolicy
+class RecipeIngredientPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
