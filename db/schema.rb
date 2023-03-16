c4d78debab0b2492722d498397328253c409dd7c
Dominic Zaleski <dominic.zaleski@outlook.com>
2023-03-16 19:30:41 +0100

    registration form completed (no css)

--- db/schema.rb
+++ db/schema.rb
@@ -134,13 +134,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_121356) do
   end
 
   create_table "recipes", force: :cascade do |t|
-    t.integer "prep_time"
+    t.time "prep_time"
     t.string "instruction"
     t.integer "difficulty"
     t.string "utensil"
     t.string "title"
-    t.integer "cooking_time"
-    t.integer "total_time"
+    t.time "cooking_time"
+    t.time "total_time"
     t.integer "serving"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
