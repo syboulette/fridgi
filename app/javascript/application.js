5d2b43c0b4f941bc61f4a1e883ff44cd1fa68eda
Sybille Lambotte <lambottesybille@gmail.com>
2023-03-18 12:13:50 +0100

    js fix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,7 +3,6 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
 //= require jquery
-//= require link_to_add_fields
 //= require popper
 //= require turbolinks
 //= require bootstrap
