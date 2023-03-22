da254db63586995b08f6d4330b93c71dc5daeb69
Vinicius Batestin <viniciusbatestin@gmail.com>
2023-03-22 21:39:43 +0100

    stash

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,9 +4,7 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :recipe_ingredients
   has_many :dietary_information_recipes
   accepts_nested_attributes_for :recipe_ingredients
-
   validates_presence_of :title, :total_time, :instruction
-  validates_presence_of :total_time, :title, :serving, :instruction, :recipe_ingredients
   has_many :users, through: :favourite_recipes
   has_many :favourite_recipes #It can be handle by the policy to only allow the owner to see/edit
 end
