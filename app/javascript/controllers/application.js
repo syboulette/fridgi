eb686a9be1f98e5f41d2cdb6479dff1b97789aa8
Sybille <115070033+syboulette@users.noreply.github.com>
2023-03-22 22:09:12 +0100

    new fix

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,3 @@
-import { Application } from "@hotwired/stimulus"
-import NestedForm from 'stimulus-rails-nested-form'
-
-
-
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
