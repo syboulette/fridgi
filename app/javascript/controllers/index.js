572191524dabd2d7c2070b9bd6ee35c67e010a59
Sybille <115070033+syboulette@users.noreply.github.com>
2023-03-22 22:06:07 +0100

    fix heroku error

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,10 +2,15 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
-
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
 import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import NestedForm from 'stimulus-rails-nested-form'
 
 const application = Application.start()
+const context = require.context(".", true, /\.js$/)
+const definitions = definitionsFromContext(context)
+
+application.load(definitions)
+application.register("hello", HelloController)
 application.register('nested-form', NestedForm)
