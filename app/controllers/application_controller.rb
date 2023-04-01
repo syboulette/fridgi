b8ecf335686c72edf2b1201fadf97de32f5a799d
Sybille <115070033+syboulette@users.noreply.github.com>
2023-04-01 11:02:09 +0200

    unsplash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit::Authorization
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  include ActiveStorage::SetCurrent
+  
   # Pundit: allow-list approach
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -1,7 +1,7 @@
 require 'unsplash'
 
 Unsplash.configure do |config|
-  config.application_access_key = 'naYbB9rov6wmeGh06p3G14ZlZxCl5iB1uMXnZOI--tk'
-  config.application_secret = '5iNZh1pj29uZ9cVM52D2mM_4eClXyB8oAOUBpkzgbZk'
-  config.utm_source = 'Fridgi'
+  config.application_access_key = 'MQh7ANfZNyRjA-Q6_aGoHxvWdHVWetFvpGpQx54et2o'
+  config.application_secret = 'Napt8hwDC2g6uFfEVeLJYhPqS8fqvxfRN3hJRmO5QlM'
+  config.utm_source = 'the_fridgi_amazing_app'
 end
