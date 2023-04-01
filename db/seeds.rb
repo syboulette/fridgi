875c574f3675c89d5f11cb5ca1901fadfefdc0b1
Sybille <115070033+syboulette@users.noreply.github.com>
2023-04-01 09:53:55 +0200

    seed edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,6 @@ start = Time.now
 
 puts "=== DESTROYING TABLES ==="
 
-User.destroy_all
-puts "User destroyed"
-
-Ingredient.destroy_all
-puts "Ingredient destroyed"
-
 #############################################
 # Ingredient
 #############################################
